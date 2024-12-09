b9d252a7b5d70e3c5ec93d49b63224ef38175a5c
Niranjan Prajapati <prajapatiniranjan8@gmail.com>
2024-12-09 20:32:33 +0530

    Updated

---
+++ 7th class/newproject/src/App.jsx
@@ -0,0 +1,15 @@
+import Child1  from "./Child1"
+ let a = prompt("Enter your name")
+
+
+function App() {
+  return (
+    <>
+
+  <Child1 Name={a}/>
+
+    </>
+  )
+}
+
+export default App
---
+++ 7th class/newproject/src/Child2.jsx
@@ -0,0 +1,9 @@
+const Child2 = (props)=>{
+    return(
+        <>
+        <h1>From Child 2 {props.fname}</h1>
+        </>
+    )
+}
+
+export default Child2
